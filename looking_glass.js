fb57f4dde821f918cc4f7e9e563b13b260b6ea9b
Chad Tolkien <chad@sodadigital.com.au>
2014-07-28 13:45:24 +1000

    fix formatting

--- looking_glass.js
+++ looking_glass.js
@@ -1,261 +1,257 @@
-function lookingGlass( container , options ){
+function lookingGlass(container, options) {
 
-	if( options == null ){ var options = { }; }
+    if (options == null) { var options = {}; }
 
-    var div = document.getElementById( container );
+    var div = document.getElementById(container);
 
     var x = div.offsetLeft;
     var y = div.offsetTop;
-	//  cursorOffset is used to set the position of the looking glass
+    //  cursorOffset is used to set the position of the looking glass
     //  viewport, relative to the mouse cursor. default is center
     var cursorOffsetX;
-   	var cursorOffsetY;
-	var centeringValue = getDeterminateLength( $(div) );
-	var modY;
-	var modX;
-	var viewportMods; // [ offset , size ]
-	var offsetMod = 1;
-	var sizeMod = null;
-	var shapeModX = 1;
-	var shapeModY = 1;
-	var radiusMod = 100;
-	var shapeMods = [ shapeModX , shapeModY , radiusMod ];
+    var cursorOffsetY;
+    var centeringValue = getDeterminateLength($(div));
+    var modY;
+    var modX;
+    var viewportMods; // [ offset , size ]
+    var offsetMod = 1;
+    var sizeMod = null;
+    var shapeModX = 1;
+    var shapeModY = 1;
+    var radiusMod = 100;
+    var shapeMods = [shapeModX, shapeModY, radiusMod];
 
-	if( options["viewportShape"] != null ){ shapeMods = getShapeMods( options["viewportShape"] ); shapeModX = shapeMods[0]; shapeModY = shapeMods[1]; }
-	if( options["viewportSize"] != null ){ viewportMods = getViewportModifier( options["viewportSize"] ); offsetMod = viewportMods[0]; sizeMod = viewportMods[1]; }
-	if( options['viewportOrientation'] != null ){
+    if (options["viewportShape"] != null) {
+        shapeMods = getShapeMods(options["viewportShape"]); shapeModX = shapeMods[0]; shapeModY = shapeMods[1];
+    }
+    if (options["viewportSize"] != null) {
+        viewportMods = getViewportModifier(options["viewportSize"]); offsetMod = viewportMods[0]; sizeMod = viewportMods[1];
+    }
+    if (options['viewportOrientation'] != null) {
 
-		var vals = getOrientationValues( options['viewportOrientation'].toUpperCase() );
-		
-		modY = vals[0]*offsetMod;
-		modX = vals[1]*offsetMod;
+        var vals = getOrientationValues(options['viewportOrientation'].toUpperCase());
 
-	}else{
+        modY = vals[0] * offsetMod;
+        modX = vals[1] * offsetMod;
 
-		modY = 4*offsetMod; 
-		modX = 4*offsetMod; 
+    } else {
+        modY = 4 * offsetMod;
+        modX = 4 * offsetMod;
+    }
 
-	}
+    cursorOffsetX = (centeringValue / shapeModY) / modX;
+    cursorOffsetY = (centeringValue / shapeModX) / modY;
 
-	cursorOffsetX = ( centeringValue / shapeModY ) / modX;
-	cursorOffsetY = ( centeringValue / shapeModX ) / modY;
+    buildTopImage();
+    buildBottomImage(sizeMod, shapeMods);
 
-	buildTopImage();
-    buildBottomImage( sizeMod , shapeMods );
+    $(document).mousemove(function (e) {
+        trackMouse(x, y, cursorOffsetX, cursorOffsetY, e);
+    });
 
-	$(document).mousemove(function ( e ){
-		
-	    trackMouse( x , y , cursorOffsetX , cursorOffsetY , e );
+    div.addEventListener('touchmove', function (e) {
+        e.preventDefault();
+        trackMouse(x, y, cursorOffsetX, cursorOffsetY, e);
 
-	});
-
-	div.addEventListener('touchmove', function ( e ) {
-
-		e.preventDefault();
-		trackMouse( x , y , cursorOffsetX , cursorOffsetY , e );
- 	
- 	}, false);
+    }, false);
 
 }// !lookingGlass()
 
-function getOrientationValues( str ){
+function getOrientationValues(str) {
 
-	var mod = []; // [0] == x , [1] == y
+    var mod = []; // [0] == x , [1] == y
 
-	// 2 : top , 100 : bottom , 4 : center(default)
-	// 2 : left , 100 : right , 4 : center(default)
-	switch( str ){
+    // 2 : top , 100 : bottom , 4 : center(default)
+    // 2 : left , 100 : right , 4 : center(default)
+    switch (str) {
 
-			case 'N':
-				mod[0] = 2;
-				mod[1] = 4;
-				break;
+        case 'N':
+            mod[0] = 2;
+            mod[1] = 4;
+            break;
 
-			case 'S':
-				mod[0] = 100;
-				mod[1] = 4;
-				break;
+        case 'S':
+            mod[0] = 100;
+            mod[1] = 4;
+            break;
 
-			case 'W':
-				mod[0] = 4;
-				mod[1] = 2;
-				break;
+        case 'W':
+            mod[0] = 4;
+            mod[1] = 2;
+            break;
 
-			case 'E':
-				mod[0] = 4;
-				mod[1] = 100;
-				break;
+        case 'E':
+            mod[0] = 4;
+            mod[1] = 100;
+            break;
 
-			default:
-				mod[0] = 0;
-				mod[1] = 0;
+        default:
+            mod[0] = 0;
+            mod[1] = 0;
 
-		}
+    }
 
-	return mod;
+    return mod;
+}
+
+function trackMouse(curX, curY, offX, offY, motionEvent) {
+
+    $("#lg-bottom-image").css({
+
+        left: motionEvent.pageX - curX - offX,
+        top: motionEvent.pageY - curY - offY,
+        'background-position': (-motionEvent.pageX + curX + offX) + 'px ' + (-motionEvent.pageY + curY + offY) + 'px'
+
+    });
 
 }
 
-function trackMouse( curX , curY , offX , offY , motionEvent ){
-	
-	$("#lg-bottom-image").css({ 
+function getViewportModifier(optionStr) {
 
-		left: motionEvent.pageX-curX-offX , 
-		top: motionEvent.pageY-curY-offY , 
-		'background-position': ( -motionEvent.pageX + curX + offX ) + 'px ' + ( -motionEvent.pageY + curY + offY ) + 'px'
+    var mods = [];
+    switch (optionStr.toUpperCase()) {
 
-	});
+        case 'SMALL':
+            mods = [2, 2];
+            break;
+
+        case 'MEDIUM':
+            mods = [1, 1];
+            break;
+
+
+        case 'LARGE':
+            mods = [.5, .5];
+            break;
+    }
+
+    return mods;
+}
+
+function getShapeMods(shapeMod) {
+
+    var modX;
+    var modY;
+    var modRadius;
+    var mods = [];
+
+    switch (shapeMod.toUpperCase()) {
+
+        case "SQUARE":
+            modX = 1;
+            modY = 1;
+            modRadius = 0;
+            break;
+
+        case "VERTICAL-RECTANGLE":
+            modX = 1;
+            modY = 2;
+            modRadius = 0;
+            break;
+
+        case "HORIZONTAL-RECTANGLE":
+            modX = 2;
+            modY = 1;
+            modRadius = 0;
+            break;
+
+        case "CIRCLE":
+
+        default:
+            modX = 1;
+            modY = 1;
+            modRadius = 100;
+
+    }
+
+    mods = [modX, modY, modRadius];
+
+    return mods;
+}
+
+function buildBottomImage(sizeMod, shapeMods) {
+
+    var imgContainer = $("#lg-bottom-image");
+    var img = imgContainer.data('src');
+
+    setImgStyles(imgContainer, img, false, sizeMod, shapeMods);
 
 }
 
-function getViewportModifier( optionStr ){
+function buildTopImage() {
 
-	var mods = [];
-	switch( optionStr.toUpperCase() ){
+    var imgContainer = $("#lg-top-image");
+    var img = imgContainer.data('src');
 
-
-		case 'SMALL':
-			mods = [ 2 , 2 ];
-			break;
-
-		case 'MEDIUM':
-			mods = [ 1 , 1 ];
-			break;
-			
-
-		case 'LARGE':
-			mods = [ .5 , .5 ];
-			break;
-			
-
-	}
-
-	return mods;
-}
-
-function getShapeMods( shapeMod ){
-
-	var modX;
-	var modY;
-	var modRadius;
-	var mods = [];
-
-	switch( shapeMod.toUpperCase() ){
-
-		case "SQUARE":
-			modX = 1;
-			modY = 1;
-			modRadius = 0;
-			break;		
-
-		case "VERTICAL-RECTANGLE":
-			modX = 1;
-			modY = 2;
-			modRadius = 0;
-			break;
-
-		case "HORIZONTAL-RECTANGLE":
-			modX = 2;
-			modY = 1;
-			modRadius = 0;
-			break;
-
-		case "CIRCLE":
-
-		default:
-			modX = 1;
-			modY = 1;
-			modRadius = 100;
-
-	}
-
-	mods = [ modX , modY , modRadius ];	
-
-	return mods;
-}
-
-function buildBottomImage( sizeMod , shapeMods ){
-
-	var imgContainer = $("#lg-bottom-image");
-	var img = imgContainer.data('src');
-
-	setImgStyles( imgContainer , img , false , sizeMod , shapeMods );
+    setImgStyles(imgContainer, img, true);
 
 }
 
-function buildTopImage(){
+function setImgStyles(container, image, isTop, sizeMod, shapeMods) {
 
-	var imgContainer = $("#lg-top-image");
-	var img = imgContainer.data('src');
+    if (isTop) {
 
-	setImgStyles( imgContainer , img , true );
+        container.css({
 
-}
+            'position': 'absolute',
+            'left': 0,
+            'top': 0,
+            'width': 100 + '%',
+            'height': 100 + '%',
+            'background': "url('" + image + "')",
+            'background-repeat': 'no-repeat',
+            'background-size': 100 + '% auto'
 
-function setImgStyles( container , image , isTop , sizeMod , shapeMods ){
+        });
 
-	if( isTop ){
+    }
+    else {
 
-		container.css({
-	
-			'position' : 'absolute' ,
-			'left' :0 ,
-			'top' :0 ,
-			'width' :100+'%' ,
-			'height' :100+'%' ,
-			'background' : "url('"+image+"')" ,
-			'background-repeat' : 'no-repeat' ,
-			'background-size' : 100+'% auto'
-	
-		});
+        var divisor = 2;
+        var shapeModY = shapeMods[1];
+        var shapeModX = shapeMods[0];
 
-	}else{
+        if (sizeMod != null) { divisor = divisor * sizeMod; }
 
-		var divisor = 2;
-		var shapeModY = shapeMods[1];
-		var shapeModX = shapeMods[0];
+        var imgDimensionVal = getDeterminateLength(container.parent()) / divisor;
+        container.css({
 
-		if( sizeMod != null ){  divisor = divisor*sizeMod; }
+            'position': 'absolute',
+            'left': 0,
+            'top': 0,
+            'width': imgDimensionVal / shapeModY,
+            'height': imgDimensionVal / shapeModX,
+            'background': "url('" + image + "')",
+            'border-radius': shapeMods[2] + '%',
+            'background-repeat': 'no-repeat',
+            'background-size': $("#lg-top-image").width() + 'px auto',
+            'overflow': 'hidden'
 
-		var imgDimensionVal = getDeterminateLength( container.parent() )/ divisor ;
-		container.css({
-	
-			'position' : 'absolute' ,
-			'left' :0 ,
-			'top' :0 ,
-			'width' : imgDimensionVal/shapeModY ,
-			'height' : imgDimensionVal/shapeModX ,
-			'background' : "url('"+image+"')" ,
-			'border-radius' : shapeMods[2]+'%' ,
-			'background-repeat' : 'no-repeat' ,
-			'background-size' : $("#lg-top-image").width() +'px auto',
-			'overflow':'hidden'
-	
-		});
+        });
 
-	}
+    }
 
 }
 
-function getDeterminateLength( container ){
+function getDeterminateLength(container) {
 
-	var boxWidth = container.width();
-	var boxHeight = container.height();
+    var boxWidth = container.width();
+    var boxHeight = container.height();
 
-	var determinateLength;
+    var determinateLength;
 
-	if( boxWidth != boxHeight ){
+    if (boxWidth != boxHeight) {
 
-		if( boxWidth < boxHeight ){
-	
-			determinateLength = boxWidth;
-	
-		}else{ determinateLength = boxHeight; }
+        if (boxWidth < boxHeight) {
 
-	}else{ 
+            determinateLength = boxWidth;
 
-		determinateLength = boxWidth; 
-	}
+        } else { determinateLength = boxHeight; }
 
-	return determinateLength;
+    } else {
+
+        determinateLength = boxWidth;
+    }
+
+    return determinateLength;
 }
